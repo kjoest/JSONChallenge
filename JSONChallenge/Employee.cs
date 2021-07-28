1cbbb6043f6875199bb2ff7853eb86d32844d454
kyle Joest <kjoest92@gmail.com>
2021-07-27 20:19:09 -0500

    first commit

---
+++ JSONChallenge/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    public class Employee
+    {
+        public int BadgeId { get; set; }
+        public List<string> ListOfDoors { get; set; }
+        public bool HasSecurityClearance { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "Kyle";
+            person.LastName = "Joest";
+            person.DOB = new DateTime(1992, 12, 17);
+            person.FavoriteIceCreamFlavors = new List<string>
+            { "Chocolate", "Cookie Dough" };
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+}
